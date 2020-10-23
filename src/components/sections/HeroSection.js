e110691f2081d7dac9a7bdbbee3735b0e5cf091a
abusiraj84 <abusiraj84@gmail.com>
2020-10-24 00:10:19 +0300

    waves

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -7,14 +7,16 @@ import PurchaseButton from "../buttons/PurchaseButton"
 // import CardsTest from "./CardsTest"
 import Card from "./Card"
 import { H1, MediumText } from "../styles/TextStyles"
+import WaveBackground from "../backgrounds/WaveBackground"
 
 function HeroSection() {
   return (
     <Wrapper>
+      <WaveBackground />
       <NavSection />
       <ContentWrapper>
         <TextWrapper>
-          <Title>Learn Design, Audio, Video, Music and much More!</Title>
+          <Title>Learn much More than Imagen!</Title>
           <Description>
             Don’t skip design. Learn design and code, by building real apps with
             React and Swift. Complete courses about the best tools.
@@ -39,15 +41,14 @@ const Wrapper = styled.div`
   width: 100%;
 `
 const ContentWrapper = styled.div`
-  background-image: url("/images/waves/livestream-wave3.svg");
-  background-repeat: no-repeat;
+  /* background-image: url("/images/waves/livestream-wave3.svg");
+  background-repeat: no-repeat; */
   overflow: hidden;
   max-width: 1234px;
   padding: 150px 30px;
   margin: 0 auto;
   @media (max-width: 640px) {
     padding: 10px 30px;
-    height: 100vh;
   }
 `
 const TextWrapper = styled.div`
